e5669a80e3daa16bd54ee26ebe111e5fcc2cbfc2
DersuG <dersu.goorskey@gmail.com>
2022-06-06 17:43:05 -0400

    added config file

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+# <https://discordpy.readthedocs.io/en/stable/quickstart.html>
+
 import discord
+import configparser
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+# Parse configuration options:
+config = configparser.ConfigParser()
+config.read('bot.ini')
+TOKEN = config['launch_options']['token']
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+# Initialize discord client:
+client = discord.Client()
 
-client = MyClient()
-client.run('my token goes here')
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('|hello'):
+        await message.channel.send('Hello!')
+
+# Run discord client:
+client.run(TOKEN)
